57b5a6d51f2519d8bd8a24a9814e9de25e9d523d
Yoav Ben Raphael <139175959+YoavBenRaph@users.noreply.github.com>
2025-08-07 16:45:48 -0400

    Add files via upload

---
+++ HandTrackingModule.py
@@ -0,0 +1,68 @@
+import cv2
+import mediapipe as mp
+import time
+
+class handDetector():
+    def __init__(self, mode=False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands #Formality needed before you can start using this model
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw =True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB) #process frame for us and give us results
+        #print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo =0, draw = True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm) #prints location of area of hand on screen
+                h, w, c = img.shape #Will give width and height
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                #print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 10, (255,0,0), cv2.FILLED)
+
+        return lmList
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()  # will give us our frame
+        img  = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()  # Gives current time
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        # Location, font, font size, font size, font color, font thickness
+        cv2.imshow("Image", img)  # Runs webcam
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
